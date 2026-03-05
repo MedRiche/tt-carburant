59fa94a24f18dc00051a311ea10f3ee8b79670ea
Mohamed010101010101 <mohamed.rich.2020.2020@gmail.com>
2026-03-05 13:34:37 +0100

    first commit

---
+++ src/main/java/com/example/ttcarburant/TtCarburantApplication.java
@@ -0,0 +1,13 @@
+package com.example.ttcarburant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtCarburantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TtCarburantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ttcarburant/TtCarburantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ttcarburant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtCarburantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
